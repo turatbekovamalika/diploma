e25827fad213f92a0e2908e32bb777b3574427ca
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-04 16:04:56 +0200

    edited the navigation to the App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
       <p>This is my diploma</p>
+
+     <nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+       
+     </nav>
+
     </div>
   );
 }
