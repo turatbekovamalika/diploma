a4e135109a225cab0466bd3c77b5b440c07055e6
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-06 14:18:04 +0200

    added new folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { NavLink, Route, Router, Routes } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <p>This is my diploma</p>
-
      <nav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
