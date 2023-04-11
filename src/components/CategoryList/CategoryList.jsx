79232a3b493a53e0f9c30ca0134f91e385677ad2
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-11 15:08:09 +0200

    created a new component that is responsible for the categories on our site

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+import "./CategoryList.css";
+
+export default function CategoryList() {
+    return (
+        <div className="CategoryList">
+            <ul>
+
+
+            </ul>
+        </div>
+    )
+}
