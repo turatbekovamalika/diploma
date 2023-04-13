9bfd7b941acf9e2aaf00c2e3417fab67f7dad47f
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-13 16:24:24 +0200

    began a edited our products

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,23 @@
+import { useContext } from "react";
 import "./ProductList.css";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 
-export default function ProductList(){
+export default function ProductList() {
+    const { products } = useContext(AppContext);
+    const output = products.map(product => (
+        <div className="Product" key={product.id}>
+            <img src={product.picture} alt={product.name} />
+            <Link to={"/product/" + product.path}>
+                {product.name}
+            </Link>
+            <span>{product.price} $</span>
+        </div>
+    ));
+
     return (
         <div className="ProductList">
-
+            {output}
         </div>
     )
 }
