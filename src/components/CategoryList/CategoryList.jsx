b7d1a088315239b706bcf5e2b27f71d64da24762
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-18 14:32:21 +0200

    began our lesson

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -6,22 +6,22 @@ import { categoryCollection } from "../../firebase";
 import { AppContext } from "../../App";
 
 export default function CategoryList() {
-    const {categories} = useContext(AppContext);
-
-    const output = categories.map(category => (
+    const { categories } = useContext(AppContext);
+    categories.sort((a, b) => b.weight - a.weight)
+    const output = categories.sort((a, b) => a.weight - b.weight).map(category => (
         <li key={category.id}>
             <NavLink to={'/category/' + category.path}>
                 {category.name}
             </NavLink>
         </li>
-      ));
-      
-      return (
+    ));
+
+    return (
         <div className="CategoryList">
             <ul>
-      
+
                 {output}
             </ul>
         </div>
-      )
-      }
+    )
+}
