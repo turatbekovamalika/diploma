a02d119c635496d193892727152b3ef2feeaad8a
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-18 14:46:37 +0200

    did a filter for category and products

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,9 +3,14 @@ import "./ProductList.css";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 
-export default function ProductList() {
+export default function ProductList({ category }) {
     const { products } = useContext(AppContext);
-    const output = products.map(product => (
+
+  
+
+    const output = products
+    .filter(product => product.category === category.id)
+    .map(product => (
         <div className="Product" key={product.id}>
             <img src={product.picture} alt={product.name} />
 
