8b29d9546401b57254f86dde0ce742a90565f11f
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-18 16:37:25 +0200

    finished our lesson

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -2,27 +2,42 @@ import "./CartList.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
+import "./CartList.css";
 
 export default function CartList() {
-    const { products, cart, setCart } = useContext(AppContext);
+  // получить продукты и содердижимое корзины
+  const { products, cart, setCart } = useContext(AppContext);
 
-    const productIds = Object.keys(cart);
-    const output = products
-        .filter(product => productIds.includes(product.id))
-        .map(product => (
-            <div className="CartItem">
-                <img src={product.picture} alt={product.name} />
-                <Link to={"/product/" + product.slug}>
-                    {product.name}
-                </Link>
-                <span>{cart[product.id]} items</span>
-                <span>{product.price * cart[product.id]} $</span>
-            </div>
-        ));
+  function onQtyChange(product, qty) {
+    setCart({
+      ...cart,
+      [product.id]: qty,
+    });
+  }
 
-    return (
-        <div className="CartList">
-            {output}
-        </div>
-    )
+  function onRemoveClick(product) {
+    const newCart = { ...cart };
+    delete newCart[product.id];
+    setCart(newCart);
+  }
+
+  const productIds = Object.keys(cart);
+  const output = products
+    .filter((product) => productIds.includes(product.id))
+    .map((product) => (
+      <div className="CartItem">
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        <input
+          type="number"
+          min={1}
+          onChange={(event) => onQtyChange(product, +event.target.value)}
+          value={cart[product.id]}
+        />
+        <span>{product.price * cart[product.id]} som</span>
+        <button onClick={() => onRemoveClick(product)}>Remove</button>
+      </div>
+    ));
+
+  return <div className="CartList">{output}</div>;
 }
