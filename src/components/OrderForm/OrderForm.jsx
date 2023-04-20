e5f4ab5b086fcce4325ad3be7784e7d3f99fadfa
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-20 16:26:58 +0200

    finished to fix orderform

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,27 +1,57 @@
-import "./OrderForm.css"
+import { addDoc } from "firebase/firestore";
+import "./OrderForm.css";
+import { orderCollection } from "../../firebase";
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { useNavigate } from "react-router-dom";
 
 export default function OrderForm() {
-    function onSubmit(event) {
-      const data = new FormData(event.target);
-  
-      event.preventDefault();
-    }
-  
-    return (
-      <div className="OrderForm">
-        <h3>Create an order</h3>
-        <form>
-          <label>
-            Name: <input type="text" name="name" required />
-          </label>
-          <label>
-            Phone: <input type="telephone" name="phone" required />
-          </label>
-          <label>
-            Address: <input type="text" name="address" required />
-          </label>
-          <button>Submit</button>
-        </form>
-      </div>
-    );
+  const { cart, setCart, user } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    // добавить данные в базу
+    addDoc(orderCollection, {
+      name: data.get("name"),
+      address: data.get("address"),
+      phone: data.get("phone"),
+      cart: cart,
+      user: user.uid,
+    }).then(() => {
+      // очистить корзину
+      setCart({});
+      // отправить пользователя на главную страницу
+      navigate("/");
+    });
   }
+
+  if (Object.keys(cart).length === 0) {
+    return "Your cart is empty.";
+  }
+
+  if (!user) {
+    return "Please login";
+  }
+
+  return (
+    <div className="OrderForm">
+      <form onSubmit={onSubmit}>
+        <h3>Create an order</h3>
+        <label>
+          Name: <input type="text" name="name" required />
+        </label>
+        <label>
+          Phone: <input type="telephone" name="phone" required />
+        </label>
+        <label>
+          Address: <input type="text" name="address" required />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
