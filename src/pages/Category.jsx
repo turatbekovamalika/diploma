b7c9371ca772f6266f0ebbe713bf2a2d139024cf
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-20 14:32:58 +0200

    use notfound page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import ProductList from "../components/ProductList/ProductList";
+import NotFound from "./NotFound";
 
 export default function Category() {
     //деструктизация 
@@ -13,6 +14,10 @@ export default function Category() {
         );
 
     
+    if (!category){
+        return<NotFound/>
+    }    
+
 
     
 
