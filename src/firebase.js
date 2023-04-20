76f1ba4f3fafe5e11d206ec00aea319553376e93
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-20 14:54:01 +0200

    changed a firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,6 +18,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 //получить список категорий (коллекция документов)
 export const categoryCollection = collection(db, 'categories');
