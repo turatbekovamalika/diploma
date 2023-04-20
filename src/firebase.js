5a41f254faf7bc332e1ee073093a3e4d22c175cb
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-20 15:01:47 +0200

    fixed our error

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
-import {getAuth} from "firebase/auth";
+import {GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,3 +23,8 @@ const auth = getAuth(app);
 //получить список категорий (коллекция документов)
 export const categoryCollection = collection(db, 'categories');
 export const produtcCollection = collection(db, 'products');
+
+const provider = new GoogleAuthProvider ();
+export const logIn = ()=> signInWithPopup(auth, provider);
+export const logOut = ()=> signOut (auth);
+export const onAuthChange = (callback)=> onAuthStateChanged(auth, callback);
