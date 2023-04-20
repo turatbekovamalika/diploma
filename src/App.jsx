55cf5f72f33c6f08f72d03424a91d30100253869
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-20 15:26:21 +0200

    I forwarded the new code via telegram

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,102 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Contacts from "./pages/Contacts";
-import Delivery from "./pages/Delivery";
 import Category from "./pages/Category";
 import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
-import { categoryCollection, produtcCollection } from "./firebase";
+import { categoryCollection, onAuthChange, productCollection } from "./firebase";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
-
 
 export const AppContext = createContext({
   categories: [],
   products: [],
 
-  //корзина
+  // корзина
   cart: {},
-  setCart: () => { },
+  setCart: () => {},
 
-  user : null,
+  user: null, // здесь будет храниться информация про пользователя
 });
 
 export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
 
+  // состояние которое хранит информацию пользователя
+  const [user, setUser] = useState(null);
 
-  //корзина
+  // корзина
   const [cart, setCart] = useState(() => {
-    //восстанавливает содержимое корзинки из памяти брауизера
+    // восстановить содержимое корзинки из памяти браузера.
     return JSON.parse(localStorage.getItem("cart")) || {};
   });
 
-
-  //выполнить эту функцию только когда содержимое корзинки меняетсяя 
+  // выполнить эту функцию только когда содержимое корзинки меняется
   useEffect(() => {
-    //сохранить содержимое корзинки в памяти блаузера
+    // сохранить содержимое корзинки в памяти браузера
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-
-
-
-  //выполнить эту функциу один раз
+  // выполнить эту функцию только один раз
   useEffect(() => {
-    //получить категроии из списка категорий
-    getDocs(categoryCollection)
-      .then(snapshot => {
-        //категории будут храниться в snapshot.docs
-        //слздать массив для категорий
-        const newCategories = [];
-        //заполнить массив данными из списка категорий
-        snapshot.docs.forEach(doc => {// doc это категория 
-          const category = doc.data();
-          category.id = doc.id;
+    // получить категории из списка категорий
+    getDocs(categoryCollection).then((snapshot) => {
+      // категории будут храниться в snapshot.docs
 
-          newCategories.push(category);
-        });
-        //задать новый массив как состояние компонента
-        setCategories(newCategories);
-      })
+      // создать массив для категорий
+      const newCategories = [];
+      // заполнить массив данными из списка категорий
+      snapshot.docs.forEach((doc) => {
+        // doc = категория
+        const category = doc.data();
+        category.id = doc.id;
 
-    //получить продукты из списка родукто
-    getDocs(produtcCollection)
-      .then(snapshot => {
-        //категории будут храниться в snapshot.docs
-        //создать массив для продуктов
-        const newProducts = [];
-        //заполнить массив данными из списка продуктов
-        snapshot.docs.forEach(doc => {// doc это категория 
-          const product = doc.data();
-          product.id = doc.id;
+        newCategories.push(category);
+      });
+      // задать новый массив как состояние комапо
+      setCategories(newCategories);
+    });
 
-          newProducts.push(product);
-        });
-        //задать новый массив как состояние компонента
-        setProducts(newProducts);
-      })
+    // получить продукты из списка продуктов
+    getDocs(productCollection).then((snapshot) => {
+      // продукты будут храниться в snapshot.docs
+
+      // создать массив для продуктов
+      const newProducts = [];
+      // заполнить массив данными из списка продвук
+      snapshot.docs.forEach((doc) => {
+        // doc = продукт
+        const product = doc.data();
+        product.id = doc.id;
+
+        newProducts.push(product);
+      });
+      // задать новый массив как состояние комапо
+      setProducts(newProducts);
+    });
+
+    onAuthChange(user => {
+      setUser(user);
+    });
   }, []);
-
-
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories, products, cart, setCart }}>
+      <AppContext.Provider value={{ categories, products, cart, setCart, user }}>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/about" element={<h2>About</h2>} />
             <Route path="/category/:path" element={<Category />} />
-            <Route path="/product/:path" element={<Product/>}/>
+            <Route path="/product/:path" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
-
 
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </AppContext.Provider>
-
-
     </div>
   );
 }
-
