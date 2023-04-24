de1574e1401fe0097f85ce8188654c171341c0f4
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-24 18:04:37 +0200

    added class name for img and product name

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -7,28 +7,33 @@ import AddToCart from "../AddToCart/AddToCart";
 export default function ProductList({ category }) {
     const { products } = useContext(AppContext);
 
-  
+
 
     const output = products
-    .filter(product => product.category === category.id)
-    .map(product => (
-        <div className="Product" key={product.id}>
-                
+        .filter(product => product.category === category.id)
+        .map(product => (
+            <div className="Product" key={product.id}>
+
                 <Link className="About-product" to={"/product/" + product.path}>
-                <img src={product.picture} alt={product.name} />
-                <div className="Tried"> <p>{product.color}</p>
-                <span>{product.price} $</span>
-                </div> 
-                <AddToCart product={product}/>
-                    { product.name }
+
+                    <div className="Ppp">
+                        <div className="Product-name">  {product.name}</div>
+                        <img src={product.picture} alt={product.name} />
+                    </div>
+
+                    <div className="Tried"> <p>{product.color}</p>
+                        <span>{product.price} $</span>
+                    </div>
+
+                    <AddToCart product={product} />
+
                 </Link>
 
-        </div>
-    ));
+            </div>
+        ));
 
     return (
         <div className="ProductList">
-           
             {output}
         </div>
     )
