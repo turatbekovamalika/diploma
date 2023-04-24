7d08d580a3a315e67f12301d191d19d9915dec6a
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-24 18:33:29 +0200

    added some h1 and p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,13 @@ export default function Home() {
         <div className="Home">
 
             <h1>Welcome to my cosmetics store</h1>
-        
-                <p>
+            <p>
                 this is the best decorative cosmetics store for your soul
             </p>
             <p>
                 We create only high-quality products so that you can empihasize your beauty
             </p>
-
-            
-               
-           
         </div>
-       
-       
-       
-        
-    )
 
-   
+    )
 }
