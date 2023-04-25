e125f5ede1c9473ba6b144d7919ef9e0d63040af
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-25 21:01:11 +0200

    delate products sort fixed for good

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -6,8 +6,11 @@ import AddToCart from "../AddToCart/AddToCart";
 
 export default function ProductList({ category }) {
     const { products } = useContext(AppContext);
-
-
+    products.sort((a, b) => {
+        const weightA = a.weight ?? 0;
+        const weightB = b.weight ?? 0;
+        return weightB - weightA;
+    })
 
     const output = products
         .filter(product => product.category === category.id)
