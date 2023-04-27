65c3c45d466dc1bc1496c51a2d5f62af05155a3d
Koshoeva Ajar <ajarka1334@gmail.com>
2023-04-27 18:10:41 +0200

    added new class name for price picture and all span

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,24 +3,34 @@ import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
 import AddToCart from "../components/AddToCart/AddToCart";
+import "./Product.css"
 
-export default function Product(){
-    const {params}= useMatch("/product/:path");
-    const {products} = useContext (AppContext);
+export default function Product() {
+  const { params } = useMatch("/product/:path");
+  const { products } = useContext(AppContext);
 
-    const product = products.find(product => product.path === params.path);
+  const product = products.find(product => product.path === params.path);
 
 
-    if(!product){
-        return<NotFound/>;
-    }
+  if (!product) {
+    return <NotFound />;
+  }
 
-    return (
-        <div className="Product">
-          <h1>{product.name}</h1>
-          <img src={product.picture} alt={product.name} />
-          <span>{product.price} som</span>
+  return (
+    <div className="Productt">
+      <h1>{product.name}</h1>
+      <div className="Picture-des">
+
+        <img src={product.picture} alt={product.name} />
+        <div className="Des-price-button">
+        <p>{product.description}</p>
+
+        <div className="Price-button">
+         <strong> <span>{product.price} $</span></strong>
           <AddToCart product={product} />
         </div>
-      );
+        </div>
+      </div>
+    </div>
+  );
 }
