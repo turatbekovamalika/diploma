4fef4a4d1fbaee72c96eb59a368d1d1420cb6157
Koshoeva Ajar <ajarka1334@gmail.com>
2023-05-03 14:32:02 +0200

    added after nuvlink components "delete category"

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -3,6 +3,8 @@ import "./CategoryList.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
+
 
 export default function CategoryList() {
     const { categories } = useContext(AppContext);
@@ -10,21 +12,22 @@ export default function CategoryList() {
 
     const output = categories.sort((a, b) => a.weight - b.weight).map(category => (
         <li key={category.id}>
-            
-            <NavLink  className="Category" to={'/category/' + category.path}>
-            <img src={category.picture} alt={category.name} />
+
+            <NavLink className="Category" to={'/category/' + category.path}>
+                <img src={category.picture} alt={category.name} />
                 {category.name}
             </NavLink>
+            <DeleteCategory category={category} />
         </li>
     ));
 
     return (
         <div className="CategoryList">
-             <p>CATEGORY</p>
-            <ul>  
+            <p>CATEGORY</p>
+            <ul>
                 {output}
             </ul>
-            <AddCategory/>
+            <AddCategory />
         </div>
     )
 }
