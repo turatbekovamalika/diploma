e3d0b4c4dd0fb792ac56849f95603bfd0e2cbf8c
Koshoeva Ajar <ajarka1334@gmail.com>
2023-05-04 14:15:39 +0200

    fixed const "drawerOpen"  and "setDrawerOpen"

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Auth from "../Auth/Auth";
 import CartLink from "../CartLink/CartLink";
-import CategoryList from "../CategoryList/CategoryList";
+import Drawer from "../Drawer/Drawer";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import NavToggle from "../NavToggle/NavToggle";
 import "./Layout.css";
 
 export default function Layout(props) {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+
+    function toggleDrawer() {
+        setDrawerOpen(!drawerOpen);
+    }
+
     return (
         <div className="Layout">
             <header>
@@ -13,7 +21,10 @@ export default function Layout(props) {
                 <Nav />
                 <Logo />
 
-                <div className="Logo-cart"> 
+                <NavToggle callback={toggleDrawer} />
+                <Drawer open={drawerOpen} toggle={toggleDrawer} />
+
+                <div className="Logo-cart">
                     <Auth />
                     <CartLink />
                 </div>
@@ -22,7 +33,7 @@ export default function Layout(props) {
 
             <main>
                 {props.children}
-                
+
             </main>
             <footer>
                 Footer
