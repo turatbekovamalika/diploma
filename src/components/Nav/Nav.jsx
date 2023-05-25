c7c4e59c07a19517fedf65e441db14d9218215da
Koshoeva Ajar <ajarka1334@gmail.com>
2023-05-25 14:24:49 +0200

    made final changes to the site

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,27 +3,23 @@ import "./Nav.css";
 
 export default function Nav() {
     let menuBtn = document.querySelectorAll('.Nav a')
-let menu = document.querySelector('.Drawer')
-menuBtn.forEach(el => {
-    el.addEventListener('click', function(){
-        menu.classList.remove('open')
+    let menu = document.querySelector('.Drawer')
+    menuBtn.forEach(el => {
+        el.addEventListener('click', function () {
+            menu.classList.remove('open')
+        })
     })
-})
-
-
-    
-    return ( 
-    
+    return (
         <div className="Nav">
-                <div className="Container">
-            <ul>
-                <li>  <NavLink to="/">Home</NavLink></li>
-                <li>  <NavLink to="/about">About</NavLink></li>
-                <li>  <NavLink to="/contacts">Contacts</NavLink></li>
-                <li>  <NavLink to="/delivery">Delivery</NavLink></li> 
-            </ul>
+            <div className="Container">
+                <ul>
+                    <li>  <NavLink to="/">Home</NavLink></li>
+                    <li>  <NavLink to="/about">About</NavLink></li>
+                    <li>  <NavLink to="/contacts">Contacts</NavLink></li>
+                    <li>  <NavLink to="/delivery">Delivery</NavLink></li>
+                </ul>
             </div>
-            </div>
+        </div>
     );
 }
 
